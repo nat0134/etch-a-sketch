c820cef989566c14a0c6dd6ba25b5c60109b801a
Natalia Lee <natalia000000@gmail.com>
2026-05-07 05:56:00 +1000

    Initial grid set up

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+
+let gridSize = 16;
+let totalSize = gridSize * gridSize;
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+
+for (let i = 1; i < width * height; i++) {
+    const grid = document.createElement("div");
+    grid.style.width = `${gridSize}px`;
+    grid.style.height = `${gridSize}px`;
+    grid.style.border = `.1px solid #808080`
+    container.appendChild(grid);
+}
