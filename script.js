1ae09c869e084da675fda886a0fb7a5ed69863f1
Natalia Lee <natalia000000@gmail.com>
2026-05-09 07:49:55 +1000

    Added hover effect event

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const container = document.getElementById("container");
+    const container = document.querySelector("#container");
 
-let gridSize = 16;
-let totalSize = gridSize * gridSize;
-const width = window.innerWidth;
-const height = window.innerHeight;
+    let gridSize = 16;
+    let totalSize = gridSize * gridSize;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
+    for (let i = 1; i < width * height; i++) {
+        const grid = document.createElement("div");
+        grid.style.width = `${gridSize}px`;
+        grid.style.height = `${gridSize}px`;
+        grid.style.border = `.1px solid #808080`;
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "gold";
+        });
+        container.appendChild(grid);
+    }
 
-for (let i = 1; i < width * height; i++) {
-    const grid = document.createElement("div");
-    grid.style.width = `${gridSize}px`;
-    grid.style.height = `${gridSize}px`;
-    grid.style.border = `.1px solid #808080`
-    container.appendChild(grid);
-}
+    
+  
