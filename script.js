9a5c1c807b211ffb33cb0aeca850c957d9e5d32d
Natalia Lee <natalia000000@gmail.com>
2026-05-10 09:30:08 +1000

    Added new function to prompt for new grid size that generates a new canvas

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
+ 
     const container = document.querySelector("#container");
+    const button = document.createElement("button");
+    button.textContent = "New Canvas";
+    document.body.appendChild(button);
+    document.body.appendChild(container);
 
     let gridSize = 16;
-    let totalSize = gridSize * gridSize;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+    let canvasSize = 400;
+    
+    function createCanvas(gridSize) {
+        container.innerHTML = "";
+        let squareSize = canvasSize / gridSize;
+        let totalSize = gridSize * gridSize;
 
-    for (let i = 1; i < width * height; i++) {
-        const grid = document.createElement("div");
-        grid.style.width = `${gridSize}px`;
-        grid.style.height = `${gridSize}px`;
-        grid.style.border = `.1px solid #808080`;
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "gold";
-        });
-        container.appendChild(grid);
+        for (let i = 0; i < totalSize; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "black";
+            });
+            container.appendChild(square);
+        }
+        
     }
+    
+    createCanvas(gridSize);
+
+    button.addEventListener("click", () => {
+        let newSize = Number(prompt("Enter a size up to 100 for new canvas!"));
+        if (newSize > 100) {
+            newSize = 100;
+        }
+        gridSize = newSize;
+        createCanvas(gridSize);
+    });
 
     
-  
+
+    
